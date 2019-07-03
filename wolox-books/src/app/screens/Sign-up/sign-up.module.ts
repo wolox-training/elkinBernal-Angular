721564847180aca44a1dd230630ad85bfa73602e
Elkin <elkin.bernal@wolox.co>
2019-07-03 09:07:13 -0500

    create login routing file

--- wolox-books/src/app/screens/Sign-up/sign-up.module.ts
+++ wolox-books/src/app/screens/Sign-up/sign-up.module.ts
@@ -3,14 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SignUpComponent } from './sign-up.component';
 import { HttpClientModule } from '@angular/common/http';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [
-  {
-      path: 'sign-up',
-      component: SignUpComponent
-  }
-]
+import { SignUpRoutingModule } from './sign-up-routing.module';
 
 @NgModule({
   declarations: [
@@ -21,7 +14,7 @@ const routes: Routes = [
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule.forChild(routes)
+    SignUpRoutingModule
   ],
   exports: [SignUpComponent]
 })
